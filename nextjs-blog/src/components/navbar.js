2b88d05f47e7017a21432d43b403e3577e2144cd
aayushi <userrendom815@gmail.com>
2024-08-29 14:48:06 +0530

    second commit

--- nextjs-blog/src/components/navbar.js
+++ nextjs-blog/src/components/navbar.js
@@ -16,53 +16,6 @@ const Navbar = () => {
 
     return (
         <>
-            {/* <AppBar position="static" sx={{ backgroundColor: "white" }}>
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="a"
-                            href="#app-bar-with-responsive-menu"
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'flex', md: 'none' },
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'black',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            LOGO
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, gap: "5px" }}>
-                            <Link href={"/"}>
-                                <Button
-                                    variant='contained'
-                                    onClick={handleHomeClick}
-                                    sx={{ my: 2, color: 'white', padding: "15px", backgroundColor: "grey" }}
-                                >
-                                    Home
-                                </Button>
-                            </Link>
-
-                            <Link href={"/reports"}>
-                                <Button
-                                    variant='contained'
-                                    onClick={handleReportClick}
-                                    sx={{ my: 2, color: 'white', padding: "15px", backgroundColor: "white", color: "green" }}
-                                >
-                                    Reports
-                                </Button>
-                            </Link>
-
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar> */}
 
             <nav >
                 <div className='bg-white w-[100%] h-[80px] flex gap-3 items-center'>
@@ -75,12 +28,6 @@ const Navbar = () => {
                     </Link>
 
                     <Link href={"/reports"}>
-                        {/* <div className='p-6 text-green-500 font-bold border-b-4 border-green-500'
-                            className = {``}
-
-                        >
-                            Reports
-                        </div> */}
                         <div
                             className={`p-6 font-bold ${activeTab === '/reports' ? 'text-green-500 border-b-4 border-green-500' : 'bg-slate-200'}`}
                             onClick={() => setActiveTab('/reports')}
